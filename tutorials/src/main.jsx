d2c34fa420077275c0f3a103dfd6608d77809cff
Hitesh <hiteshkasina444@gmail.com>
2026-05-27 22:48:25 +0530

    git_tutorial first commit

---
+++ tutorials/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import {createRoot} from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+const root=createRoot(document.querySelector('#root'));
+root.render(<p>Hello ! Welcome to the React World</p>)
